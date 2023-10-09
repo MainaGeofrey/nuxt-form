af5e5047647a96056acf314da8ec467877c1f64c
@jeff <jeffdevops6@gmail.com>
2023-10-09 10:19:29 +0300

    schedule callback, sel serve

--- components/steps/Step3.vue
+++ components/steps/Step3.vue
@@ -1,42 +1,64 @@
 <template>
-    <div class=" w-full max-w-[720px] mx-auto flex flex-col items-start space-y-10 ">
-       <div class="w-full max-w-[648px] space-y-3 ">
-            <div class=" w-full border-2 border-[#ccc] py-4 px-4 cursor-pointer">
-                <span class=" text-lg text-[#333]">Schedule a Call Back</span>
-                <p class=" text-sm text-[#666]">Book a virtual appointment with an expert for an expedited pre-approval tailored to your specific needs</p>
+    <div class="w-full max-w-[720px] mx-auto flex flex-col items-start space-y-10">
+        <div class="w-full max-w-[648px] space-y-3">
+            <div>
+                <input type="radio" id="hosting-big-callback" 
+                name="hosting"
+                value="hosting-big"
+                class="hidden peer"
+                @click="handleClick('callback')"
+                />
+                <label for="hosting-big-callback" 
+                    class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200
+                    rounded-lg cursor-pointer dark:hover:text-gray-300 dark:peer-checked:text-blue-600
+                    peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100
+                    dark:text-gray-400"
+                    >
+                    <div class="block">
+                        <span class="text-lg text-gray-700">Schedule a Call Back</span>
+                        <p class="text-sm text-gray-600">Book a virtual appointment with an expert for an expedited
+                            pre-approval tailored to your specific needs</p>
+                    </div>
+                </label>
             </div>
-             <div class=" w-full border-2 border-[#ccc] py-4 px-4 cursor-pointer">
-                <span class=" text-lg text-[#333]">Self-Serve</span>
-                <p class=" text-sm text-[#666]">Enjoy a hassle-free experience using our platform and complete your pre-approval at your own pace</p>
-            </div>
-       </div>
 
-       <div class=" flex space-x-4 items-center" @click="next">
-                 <button type="button" class="inline-flex w-full justify-center rounded-md bg-red-600 px-6 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 ">Continue</button>
-                 <a  class=" text-red-500 flex space-x-2 whitespace-nowrap" href="#"> <span>Press</span>  <span class=" font-bold">Enter ↵</span></a>
-       </div>
+            <div>
+                <input type="radio" id="hosting-big-self-serve" 
+                name="hosting"
+                value="hosting-big"
+                class="hidden peer"
+                @click="handleClick('self-serve')" 
+                />
+                <label for="hosting-big-self-serve" 
+                    class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200
+                    rounded-lg cursor-pointer dark:hover:text-gray-300 dark:peer-checked:text-blue-600
+                    peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100
+                    dark:text-gray-400"
+                    >
+                    <div class="block">
+                        <span class="text-lg text-gray-700">Self-Serve</span>
+                        <p class="text-sm text-gray-600">Enjoy a hassle-free experience using our platform and complete your
+                            pre-approval at your own pace</p>
+                    </div>
+                </label>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup>
-import OptionSelectVue from "@/components/UI/OptionSelect.vue"
- const emit = defineEmits(['nextStep']);
+import OptionRadio from "@/components/UI/OptionRadio.vue";
+
+const emit = defineEmits(['nextStep']);
+let selectedOption = null; 
 
 const next = () => {
     emit('nextStep');
-}
+};
 
-
-onMounted(() => {
-    //on click enter go next
-    document.addEventListener('keydown', (e) => {
-        if (e.key === 'Enter') {
-            next()
-        }
-    })
-})
-
-
+const handleClick = (option) => {
+    selectedOption = option;
+    next();
+};
 </script>
-
 
